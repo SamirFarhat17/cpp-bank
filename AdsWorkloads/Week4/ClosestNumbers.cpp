6c4dec3d26d3c1acd831c6f9832e475146f10136
Samir Farhat <samirfarhat17@gmail.com>
2025-03-23 16:14:30 -0400

    Week4 Progress

---
+++ AdsWorkloads/Week4/ClosestNumbers.cpp
@@ -0,0 +1,137 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+/*
+ * Complete the 'closestNumbers' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts INTEGER_ARRAY arr as parameter.
+ */
+ 
+ 
+void merge(vector<int>& arr, vector<int>& ls, vector<int>& rs, int left, int right) {
+    int l = 0, r = 0, k = 0;
+    while(l < left && r < right) {
+        if(ls[l] < rs[r]) {
+            arr[k++] = ls[l++];
+        }
+        else arr[k++] = rs[r++];
+    }
+    while(l < left) arr[k++] = ls[l++];
+    while(r < right) arr[k++] = rs[r++];
+    
+}
+
+void mergeSort(vector<int>& arr) {
+    int n = arr.size();
+    if(n < 2) return;
+    
+    int mid = n/2;
+    
+    vector<int> ls, rs;
+    for(int i = 0; i < mid; i++) ls.push_back(arr[i]);
+    for(int i = mid; i < n; i++) rs.push_back(arr[i]);
+    
+    mergeSort(ls);
+    mergeSort(rs);
+    merge(arr, ls, rs, static_cast<int>(ls.size()), static_cast<int>(rs.size()));
+}
+
+vector<int> closestNumbers(vector<int>& arr) {
+    mergeSort(arr);
+    long smallestDif = LONG_MAX;
+    vector<int> res(2);
+    for(int i = 0; i < static_cast<int>(arr.size()-1); i++) {
+        if(std::abs(arr[i] - arr[i+1]) < smallestDif) {
+            smallestDif = std::abs(arr[i] - arr[i+1]);
+            res[0] = arr[i];
+            res[1] = arr[i+1];
+        }
+    }
+    return res;
+}
+
+
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string n_temp;
+    getline(cin, n_temp);
+
+    int n = stoi(ltrim(rtrim(n_temp)));
+
+    string arr_temp_temp;
+    getline(cin, arr_temp_temp);
+
+    vector<string> arr_temp = split(rtrim(arr_temp_temp));
+
+    vector<int> arr(n);
+
+    for (int i = 0; i < n; i++) {
+        int arr_item = stoi(arr_temp[i]);
+
+        arr[i] = arr_item;
+    }
+
+    vector<int> result = closestNumbers(arr);
+
+    for (size_t i = 0; i < result.size(); i++) {
+        fout << result[i];
+
+        if (i != result.size() - 1) {
+            fout << " ";
+        }
+    }
+
+    fout << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
+
+vector<string> split(const string &str) {
+    vector<string> tokens;
+
+    string::size_type start = 0;
+    string::size_type end = 0;
+
+    while ((end = str.find(" ", start)) != string::npos) {
+        tokens.push_back(str.substr(start, end - start));
+
+        start = end + 1;
+    }
+
+    tokens.push_back(str.substr(start));
+
+    return tokens;
+}
